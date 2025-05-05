1aab4fff8e486004d9a70c99dbcf08018203a235
Isaac Jordan <isaaz.jordan02@gmail.com>
2025-05-04 22:18:37 -0300

    Add : Provide Animations for pagination to work

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import { AppComponent } from './app/app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app/app-routing.module';
 import {importProvidersFrom} from '@angular/core';
+import {provideAnimations} from '@angular/platform-browser/animations';
 
 bootstrapApplication(AppComponent, {
   providers: [
     importProvidersFrom(HttpClientModule),
-    ...AppRoutingModule
+    ...AppRoutingModule,
+    provideAnimations()
   ]
 }).catch(err => console.error(err));
