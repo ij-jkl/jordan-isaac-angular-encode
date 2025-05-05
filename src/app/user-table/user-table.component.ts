b7bf73691408db09d8bf1fdc4bf72b51c856ccf8
Isaac Jordan <isaaz.jordan02@gmail.com>
2025-05-04 22:03:31 -0300

    Add : Pagination for the table

--- src/app/user-table/user-table.component.ts
+++ src/app/user-table/user-table.component.ts
@@ -1,16 +1,19 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TableModule } from 'primeng/table';
+import {PaginatorModule} from 'primeng/paginator';
 import {User} from '../models/user.model';
 
 @Component({
   selector: 'app-user-table',
   standalone: true,
-  imports: [CommonModule,TableModule],
+  imports: [CommonModule,TableModule,PaginatorModule],
   templateUrl: './user-table.component.html',
   styleUrls: ['./user-table.component.css']
 })
 
 export class UserTableComponent {
   @Input() users: User[] = [];
+
+
 }
