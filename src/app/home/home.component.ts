a183e2682f5a18357072fbe904bc354305b13480
Isaac Jordan <isaaz.jordan02@gmail.com>
2025-05-04 22:34:59 -0300

    Refactor : Clean up of project and added body to style it later.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,6 +12,7 @@ import {User} from '../models/user.model';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent implements OnInit {
   users: User[] = [];
   filteredUsers: User[] = [];
