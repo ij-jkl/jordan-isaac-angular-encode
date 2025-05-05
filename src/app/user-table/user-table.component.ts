8623e9e8bc7d112b96733b368631797f3bd49e3a
Isaac Jordan <isaaz.jordan02@gmail.com>
2025-05-04 21:39:34 -0300

    Styles : Adding and modifying styles, changed table to primeng Table

--- src/app/user-table/user-table.component.ts
+++ src/app/user-table/user-table.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TableModule } from 'primeng/table';
 import {User} from '../models/user.model';
 
 @Component({
   selector: 'app-user-table',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,TableModule],
   templateUrl: './user-table.component.html',
   styleUrls: ['./user-table.component.css']
 })
